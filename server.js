c9018fd2fd48609f90fa69abc00f7d59e5533022
gilpop8663 <wolfye@naver.com>
2023-01-05 06:01:01 +0900

    feat(db,package): 데이터 베이스 정보와 json-server 설치

---
+++ server.js
@@ -0,0 +1,36 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
+const port = process.env.PORT || 3000;
+const { users, messages } = require('../db.json');
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
+server.use(jsonServer.bodyParser);
+
+server.post('/login', (req, res) => {
+  console.log(req.body);
+  const { id, password } = req.body;
+  const userInfo = users.find(e => {
+    return e.id === id && e.password === password;
+  });
+  if (userInfo) {
+    res.send(userInfo);
+  } else {
+    res.send();
+  }
+});
+
+server.use(router);
+
+server.listen(port, () => {
+  console.log(port);
+  console.log('server is running');
+});
